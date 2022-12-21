59a2e984db6344d66d29bc0debada431889c71d9
Konstantin Georgiev <92094692+Konstantin204@users.noreply.github.com>
2022-12-21 16:19:05 +0200

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,53 +26,63 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter prime number p:");
         int p = scanner.nextInt();
+
         System.out.println("Enter prime number q:");
         int q = scanner.nextInt();
+
         if(isPrime(p)) System.out.println("Your number p is not prime!");
         if(isPrime(q)) System.out.println("Your number q is not prime!");
+
         int n = p*q;
         System.out.println("the value of n: " + n);
+
         int phiN = (p-1)*(q-1);
         System.out.println("the value of phiN: " + phiN);
+
         int e;
         for(e = 2 ; e<phiN ; e++){
             if(gcd(e , phiN) == 1)
                 break;
         }
         System.out.println("the value of e: " + e);
-        int d = 0;
-        for(int i = 0 ; i <= 9 ; i++){
-            int x = 1 + (i * phiN);
-            if(x % e == 0)
-            {
-                d = x / e;
-                break;
-            }
-        }
+
+        BigInteger E = BigInteger.valueOf(e);
+        BigInteger PhiN = BigInteger.valueOf(phiN);
+        BigInteger d = E.modInverse(PhiN);
         System.out.println("the value of d: " + d);
+
         Scanner scannerMsg = new Scanner(System.in);
         System.out.println("Enter message to be encrypted: ");
         String cipher = scannerMsg.nextLine();
+
         BigInteger[] cipherAscii = new BigInteger[cipher.length()];
         for(int i = 0; i < cipher.length(); i++){
             cipherAscii[i] = BigInteger.valueOf(cipher.charAt(i));
         }
+
         BigDecimal[] c = new BigDecimal[cipherAscii.length];
         BigInteger N = BigInteger.valueOf(n);
+
         for(int i = 0; i < cipherAscii.length; i++){
             c[i] = new BigDecimal(cipherAscii[i].pow(e).mod(N), 0);
             System.out.println(c[i]);
         }
+
         BigInteger[] decryptedAscii = new BigInteger[c.length];
+
         for(int i = 0; i < c.length; i++){
-            decryptedAscii[i] = c[i].toBigInteger().pow(d).mod(N);
+            decryptedAscii[i] = c[i].toBigInteger().pow((d.intValue())).mod(N);
         }
+
         char[] decrypted = new char[decryptedAscii.length];
+
         for(int i = 0; i < decryptedAscii.length; i++){
             decrypted[i] = (char)(decryptedAscii[i]).intValueExact();
         }
+
         for(int i = 0; i < decrypted.length; i++){
             System.out.println(decrypted[i]);
         }
